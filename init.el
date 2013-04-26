cc45062023a84fe22772881a2c596bf492d1b983
Jonas Gorauskas <jgorauskas@gmail.com>
2013-04-26 16:19:08 -0700

    added unicode support

--- init.el
+++ init.el
@@ -125,3 +125,24 @@
 
 (add-to-list 'recentf-filename-handlers 'abbreviate-file-name)
 (add-to-list 'auto-mode-alist '("~/.emacs.d/thirdparty/yasnippet/snippets/.*" . snippet-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Unicode enabled everywhere
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+(setq locale-coding-system 'utf-8)
+
+(if (boundp 'buffer-file-coding-system)
+    (setq-default buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+(if (eq system-type 'windows-nt)
+    (set-selection-coding-system 'utf-16-le)
+  (set-selection-coding-system 'utf-8))
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
