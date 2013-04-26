130dba61022df4557d0748a314359479ede77657
Jonas Gorauskas <jgorauskas@gmail.com>
2013-04-26 16:16:57 -0700

    added init header

---
+++ init.el
@@ -0,0 +1,7 @@
+;; GNU Emacs init for Jonas Gorauskas
+;; Modified: 2013-04-26 16:16 by jgg
+;; http://jonas.gorauskas.com/
+;; http://www.thestandardoutput.com/
+;; Copyright 1997-2013 by Jonas Gorauskas
+
+(message "This is the JGG EMACS INIT")
