281d29dd5cb302bf667534510df1650645cb8ecc
Jonas Gorauskas <jgorauskas@gmail.com>
2013-05-03 03:14:10 -0700

    tweaks to markdown mode

--- jgg-code.el
+++ jgg-code.el
@@ -1,5 +1,5 @@
 ;; This file is part of emacs init for Jonas Gorauskas
-;; modified: 2013-05-02 18:10 by jgg
+;; modified: 2013-05-03 03:08 by jgg
 ;; http://jonas.gorauskas.com/
 ;; http://www.thestandardoutput.com/
 
@@ -10,7 +10,10 @@
 (require 'markdown-mode)
 
 ;(setq markdown-command "pandoc --from=markdown --to=html5 --standalone")
-(setq markdown-command "markdown_py")
+(setq markdown-command "markdown"
+      markdown-css-path "notes.css"
+      markdown-coding-system 'utf-8
+      markdown-content-type "text/html")
 
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
