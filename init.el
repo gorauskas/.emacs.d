ddcbfd4e5820196617bb1c6ca5a659b43e9b74ae
Jonas Gorauskas <jgorauskas@gmail.com>
2014-01-19 13:32:02 -0800

    added save history logic

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 ;; GNU Emacs init for Jonas Gorauskas
-;; Modified: 2013-12-06 03:09 by jgg
+;; Modified: 2014-01-19 13:30 by jgg
 ;; http://jonas.gorauskas.com/
 ;; http://www.thestandardoutput.com/
-;; Copyright 1997-2013 by Jonas Gorauskas
+;; Copyright 1997-2014 by Jonas Gorauskas
 
 (message "JGG: This is EMACS INIT")
 
@@ -94,6 +94,8 @@
                                          try-expand-line
                                          try-complete-lisp-symbol-partially
                                          try-complete-lisp-symbol)
+      history-length 4096
+      history-delete-duplicates t
       inhibit-startup-screen t
       initial-scratch-message ";; Happy Hacking!\n\n"
       make-backup-files t
@@ -107,6 +109,7 @@
       recentf-max-saved-items 100
       require-final-newline t
       save-interprogram-paste-before-kill t
+      savehist-mode t
       scroll-margin 0
       scroll-conservately 100000
       scroll-up-aggressively 0
@@ -114,7 +117,7 @@
       scroll-preserve-screen-position t
       search-highlight t
       show-paren-style 'mixed
-      smex-history-length 10
+      smex-history-length 4096
       smex-save-file "~/.emacs.d/.smex-items"
       speedbar-directory-unshown-regexp "^$"
       speedbar-show-unknown-files t
