8705617959e1640f1aa93c89b6194793e2028126
Jonas Gorauskas <jgorauskas@gmail.com>
2014-01-29 04:15:23 +0000

    fixed desktop session lock error

--- jgg-session.el
+++ jgg-session.el
@@ -1,5 +1,5 @@
 ;; This file is part of the emacs init for Jonas Gorauskas
-;; modified: 2013-12-05 02:02 by jgg
+;; modified: 2014-01-28 17:39 by jonasg211
 ;; http://jonas.gorauskas.com/
 ;; http://www.thestandardoutput.com/
 
@@ -16,6 +16,25 @@
       desktop-files-not-to-save "^$"
       desktop-load-locked-desktop nil)
 (desktop-save-mode 1)
+
+(defun emacs-process-p (pid)
+  "If PID is the PID of an emacs process, return t, else nil.
+   Also return nil if PID is nil."
+  (when pid
+    (let ((attributes (process-attributes pid)) (cmd))
+      (dolist (attr attributes)
+        (if (string= "comm" (car attr))
+            (setq cmd (cdr attr))))
+      (if (and cmd
+               (or (string= "emacs" cmd)
+                   (string= "emacs.exe" cmd)))
+          t))))
+
+(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
+  "Don't allow dead emacs to own the desktop file"
+  (when (not (emacs-process-p ad-return-value))
+    (setq ad-return-value nil)))
+
 
 ;; Save the cursor position within files
 (require 'saveplace)
